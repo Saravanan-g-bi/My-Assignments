931eb7314ca385924c8c5610358906f08f8bba9e
Saravanan G <62875615+Saravanan-g-bi@users.noreply.github.com>
2025-04-27 10:46:31 +0530

    Add files via upload
    Day 1 Assignments

---
+++ day1/chrome.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class chrome {
+	public static void main(String[] args) {
+		float a=91.0f;
+		System.out.println(a);
+		String name="Google";
+		System.out.println(name);
+		boolean m = false;
+		System.out.println(m);
+		int year=2008;
+		System.out.println(year);
+		char value='C';
+		System.out.println(value);
+			
+	}
+
+}
